1ecf229cd3a0397b7c688d9a45ecf9c870478eb0
Izabela Jabłecka <izjab@icloud.com>
2023-12-10 22:14:43 +0100

    added styles and modified HomeHeader

--- src/components/HomeHeader.jsx
+++ src/components/HomeHeader.jsx
@@ -1,32 +1,31 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import {Link} from 'react-router-dom';
 import '../scss/HomeHeader.scss'
 
 const HomeHeader = () => {
     return (
-        <>
-        <div className='login'>
-            <a href={"/"}> Zaloguj</a>
-            <a href={"/"}> Załóż konto</a>
-        </div>
-    <nav>
-        <a href="/">Start</a>
-        <a href="/">O co chodzi?</a>
-        <a href="/">O nas</a>
-        <a href="/">Fundacja i organizacje</a>
-        <a href="/">Kontakt</a>
-    </nav>
-            <img className='img-box' src="src/assets/images/Hero Image.png" alt="" align='left'/>
+        <header>
+            <div className='login'>
+                <a className='' href={"/"}> Zaloguj</a>
+                <a href={"/"}> Załóż konto</a>
+            </div>
+            <nav>
+                <a href="/">Start</a>
+                <a href="/">O co chodzi?</a>
+                <a href="/">O nas</a>
+                <a href="/">Fundacja i organizacje</a>
+                <a href="/">Kontakt</a>
+            </nav>
+            <img className="img-box" src="src/assets/images/Hero Image.png" alt="" align='left'/>
             <div>
-            <h1>Zacznij pomagać! Oddaj niechciane rzeczy w zaufane ręce</h1>
-            <img className='decoration' src="src/assets/images/Decoration.png" alt=""/>
+                <h1>Zacznij pomagać! <br/> Oddaj niechciane rzeczy w zaufane ręce</h1>
+                <img className='decoration' src="src/assets/images/Decoration.png" alt=""/>
             </div>
             <div>
                 <a href="/">Oddaj rzeczy</a>
                 <a href="/">Zorganizuj zbiórkę</a>
             </div>
-            
-            </>
+        </header>
     );
 }
 
