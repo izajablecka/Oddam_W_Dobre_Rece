6a3df6af47622d369bd199ec324944cb5718c40a
Izabela Jabłecka <izjab@icloud.com>
2023-12-12 15:09:28 +0100

    added some styles to HomeSimpleSteps

--- src/components/HomeSimpleSteps.jsx
+++ src/components/HomeSimpleSteps.jsx
@@ -5,6 +5,7 @@ import Tshirt from '../assets/images/Icon-1.png'
 import Bag from '../assets/images/Icon-2.png'
 import Search from '../assets/images/Icon-3.png'
 import Reload from '../assets/images/Icon-4.png'
+import "../scss/HomeSimpleSteps.scss"
 
 
 const HomeSimpleSteps = () => {
@@ -15,31 +16,32 @@ const HomeSimpleSteps = () => {
                 <h1 className="title__hdl">Wystarczą 4 proste kroki</h1>
                 <img className="title__img" src={decoration} alt="dekoracja"/>
             </div>
+            <section className='simple-steps'>
+                <div className="col">
+                    <img className="col__img col__img--1" src={Tshirt} alt="Tshirt"/>
+                    <h4 className="col__hdl">Wybierz rzeczy</h4>
+                    <p className="col__dsc">ubrania, zabawki, sprzęt i inne</p>
+                </div>
 
-            <div className="col">
-                <img className="col__img col__img--1" src={Tshirt} alt="Tshirt"/>
-                <h4 className="col__hdl">Wybierz rzeczy</h4>
-                <p className="col__dsc">ubrania, zabawki, sprzęt i inne</p>
-            </div>
-
-            <div className="col">
-                <img className="col__img col__img--2" src={Bag} alt="Tshirt"/>
-                <h4 className="col__hdl">Wybierz rzeczy</h4>
-                <p className="col__dsc">ubrania, zabawki, sprzęt i inne</p>
-            </div>
+                <div className="col">
+                    <img className="col__img col__img--2" src={Bag} alt="Tshirt"/>
+                    <h4 className="col__hdl">Spakuj je</h4>
+                    <p className="col__dsc">skorzystaj z worków na śmieci</p>
+                </div>
 
 
-            <div className="col">
-                <img className="col__img col__img--3" src={Search} alt="Tshirt"/>
-                <h4 className="col__hdl">Wybierz rzeczy</h4>
-                <p className="col__dsc">ubrania, zabawki, sprzęt i inne</p>
-            </div>
+                <div className="col">
+                    <img className="col__img col__img--3" src={Search} alt="Tshirt"/>
+                    <h4 className="col__hdl">Zdecyduj komu chcesz pomóc</h4>
+                    <p className="col__dsc">ubrania, zabawki, sprzęt i inne</p>
+                </div>
 
-            <div className="col">
-                <img className="col__img col__img--4" src={Reload} alt="Tshirt"/>
-                <h4 className="col__hdl">Wybierz rzeczy</h4>
-                <p className="col__dsc">ubrania, zabawki, sprzęt i inne</p>
-            </div>
+                <div className="col">
+                    <img className="col__img col__img--4" src={Reload} alt="Tshirt"/>
+                    <h4 className="col__hdl">Zamów kuriera</h4>
+                    <p className="col__dsc">kurier przyjedzie w dogodnym terminie</p>
+                </div>
+            </section>
             <div className="btn">
                 <Link to="/Login" className="btn__steps">ODDAJ RZECZY</Link>
             </div>
--- src/components/HomeThreeColumns.jsx
+++ src/components/HomeThreeColumns.jsx
@@ -6,26 +6,26 @@ const HomeThreeColumns = () => {
         <section>
             <section className="columns">
 
-                <div className="col">
-                    <h1 className="col__number col__number--1">10</h1>
-                    <h4 className="col__hdl">ODDANYCH WORKÓW</h4>
-                    <p className="col__dsc"> Lorem ipsum dolor sit amet, consectetur adipisc Pellentesque vel enim a
+                <div className="column">
+                    <h1 className="column__number column__number--1">10</h1>
+                    <h4 className="column__hdl">ODDANYCH WORKÓW</h4>
+                    <p className="column__dsc"> Lorem ipsum dolor sit amet, consectetur adipisc Pellentesque vel enim a
                         elit
                         viverra elementuma. Aliquam erat volutpat. </p>
                 </div>
 
-                <div className="col">
-                    <h1 className="col__number col__number--2">5</h1>
-                    <h4 className="col__hdl">WSPARTYCH ORGANIZACJI</h4>
-                    <p className="col__dsc"> Lorem ipsum dolor sit amet, consectetur adipisc Pellentesque vel enim a
+                <div className="column">
+                    <h1 className="column__number column__number--2">5</h1>
+                    <h4 className="column__hdl">WSPARTYCH ORGANIZACJI</h4>
+                    <p className="column__dsc"> Lorem ipsum dolor sit amet, consectetur adipisc Pellentesque vel enim a
                         elit
                         viverra elementuma. Aliquam erat volutpat. </p>
                 </div>
 
-                <div className="col">
-                    <h1 className="col__number col__number--3">7</h1>
-                    <h4 className="col__hdl">ZORGANIZOWANYCH ZBIÓREK</h4>
-                    <p className="col__dsc"> Lorem ipsum dolor sit amet, consectetur adipisc Pellentesque vel enim a
+                <div className="column">
+                    <h1 className="column__number column__number--3">7</h1>
+                    <h4 className="column__hdl">ZORGANIZOWANYCH ZBIÓREK</h4>
+                    <p className="column__dsc"> Lorem ipsum dolor sit amet, consectetur adipisc Pellentesque vel enim a
                         elit
                         viverra elementuma.
                         Aliquam erat volutpat. </p>
