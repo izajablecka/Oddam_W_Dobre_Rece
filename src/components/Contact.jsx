12e79390637c97753b8c732f94fe36c0c60301d1
Izabela Jabłecka <izjab@icloud.com>
2023-12-20 22:36:33 +0100

    some changes

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import '../scss/Contact.scss'
 import dekoracja from "../assets/images/Decoration.png";
+import facebook from "../assets/images/Facebook.png"
+import instagram from "../assets/images/Instagram.png"
 
 const Contact = () => {
 
@@ -10,21 +12,27 @@ const Contact = () => {
                 <div className="contact__hdl">Skontaktuj się z nami</div>
                 <img className="contact__decoration" src={dekoracja} alt=""/>
                 <form className="contact__form">
-                    <label>
+                    <label className="name">
                         Wpisz swoje imię
                         <input type="text" name="name"/>
                     </label>
-                    <label>
+                    <label className="email">
                         Wpisz swój email
                         <input type="email" name="email"/>
                     </label>
-                    <label>
+                    <label className="message">
                         Wpisz swoją wiadomość
                         <textarea name="message" rows="4"/>
                     </label>
                     <button type="submit">Wyślij</button>
                 </form>
             </div>
+            <div className="copyright">Copyright by Coders Lab</div>
+            <div className="icons">
+                <img className="icon" src={facebook} alt=""/>
+                <img className="icon" src={instagram} alt=""/>
+            </div>
+
         </div>
 
     )
