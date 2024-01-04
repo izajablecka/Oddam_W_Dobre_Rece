051972f615425a80df37ec6f9c1be15376b1f827
Izabela Jabłecka <izjab@icloud.com>
2024-01-04 10:26:40 +0100

    img path

--- src/components/HomeHeader.jsx
+++ src/components/HomeHeader.jsx
@@ -3,6 +3,8 @@ import { Link as RouterLink } from 'react-router-dom';
 import { Link as ScrollLink } from 'react-scroll';
 import '../scss/HomeHeader.scss'
 import hero from "../assets/images/Hero Image.png"
+import dekoracja from "../assets/images/Decoration.png"
+
 
 const HomeHeader = () => {
 
@@ -28,7 +30,7 @@ const HomeHeader = () => {
                 </div>
                 <div className="text-container">
                     <h2>Zacznij pomagać! <br/> Oddaj niechciane rzeczy w zaufane ręce</h2>
-                    <img className='decoration' src="../assets/images/Decoration.png" alt=""/>
+                    <img className='decoration' src={dekoracja} alt=""/>
                     <div className="collect-container">
                         <RouterLink className='collect' to="/login">Oddaj <br /> rzeczy</RouterLink>
                         <RouterLink className='collect' to="/login">Zorganizuj <br /> zbiórkę</RouterLink>
